01d55e768d137f33047c52a6587da94ea799406f
Juan Salazar <juan.salazar@grability.com>
2016-12-20 11:03:38 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  keyframes
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('animate', [
+      state('bounceOutUp', style({})),
+      transition('* => bounceOutUp', [
+        animate(800, keyframes([
+          style({ transform: ' translate3d(0, -10px, 0)', offset: 0.2 }),
+          style({ opacity: 1, transform: 'translate3d(0, 20px, 0)', offset: 0.5 }),
+          style({ opacity: 0, transform: 'translate3d(0, -2000px, 0)', offset: 1 })
+        ]))
+      ]),
+      state('bounceInRight', style({})),
+      transition('* => bounceInRight', [
+        animate(800, keyframes([
+          style({ opacity: 0, transform: 'translate3d(3000px, 0, 0)', offset: 0.2 }),
+          style({ opacity: 1, transform: 'translate3d(-25px, 0, 0)', offset: 0.4 }),
+          style({ opacity: 1, transform: 'translate3d(10px, 0, 0)', offset: 0.5 }),
+          style({ opacity: 1, transform: 'translate3d(-5px, 0, 0)', offset: 1 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app works!';
+
+  public state = '';
+
+  animate(state: string) {
+    this.state = state;
+  }
 }
